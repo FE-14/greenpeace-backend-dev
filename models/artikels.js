ba96603002f72a61824c690113aac9001e546d60
dadang17 <iskandardzulqornain38@gmail.com>
2022-11-30 21:13:23 +0700

    error fixing

--- models/artikels.js
+++ models/artikels.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Artikels extends Model {
     /**
@@ -13,19 +11,21 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Artikels.init({
-    title: DataTypes.STRING,
-    authorName: DataTypes.STRING,
-    postDescription: DataTypes.TEXT,
-    postContent: DataTypes.TEXT,
-    tags_1: DataTypes.STRING,
-    tags_2: DataTypes.STRING,
-    tags_3: DataTypes.STRING,
-    imageUrl: DataTypes.STRING,
-    postDate: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Artikels',
-  });
+  Artikels.init(
+    {
+      title: DataTypes.STRING,
+      authorName: DataTypes.STRING,
+      postDescription: DataTypes.TEXT,
+      postContent: DataTypes.TEXT,
+      tags_1: DataTypes.STRING,
+      tags_2: DataTypes.STRING,
+      tags_3: DataTypes.STRING,
+      imageUrl: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Artikels",
+    }
+  );
   return Artikels;
 };
